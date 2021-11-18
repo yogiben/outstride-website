2c8cda2eecd9eaee0185e915f35a7517560265d1
Ben Jones <benjaminpeterjonesesquire@gmail.com>
2021-11-18 19:43:48 +0100

    Small fixes

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# outstride-website
+# Oustride Website
+https://www.outstride.co
+`python -m SimpleHTTPServer`
+`sass --watch sass/stylesheet.scss css/stylesheet.css`
--- js/theme.js
+++ js/theme.js
@@ -311,78 +311,86 @@ var submit = $('#submit-btn'); // submit button
 form.on('submit', function (e) {
 	e.preventDefault(); // prevent default form submit
 
-	if (typeof $('#google-recaptcha-v3').val() != "undefined") {
-		grecaptcha.ready(function () {
-			var site_key = $('#google-recaptcha-v3').attr('src').split("render=")[1];
-			grecaptcha.execute(site_key, {action: 'contact'}).then(function (token) {
-				var gdata = form.serialize() + '&g-recaptcha-response=' + token;
-				$.ajax({
-					url: 'php/mail.php',  // form action url
-					type: 'POST', 		  // form submit method get/post
-					dataType: 'json', 	  // request type html/json/xml
-					data: gdata, 		  // serialize form data
-					beforeSend: function () {
-						submit.attr("disabled", "disabled");
-						var loadingText = '<span role="status" aria-hidden="true" class="spinner-border spinner-border-sm align-self-center me-2"></span>Sending.....'; // change submit button text
-						if (submit.html() !== loadingText) {
-							submit.data('original-text', submit.html());
-							submit.html(loadingText);
-						}
-					},
-					success: function (data) {
-						submit.before(data.Message).fadeIn("slow"); // fade in response data 
-						submit.html(submit.data('original-text'));// reset submit button text
-						submit.removeAttr("disabled", "disabled");
-						if (data.response == 'success') {
-							form.trigger('reset'); // reset form
-						}
-						setTimeout(function () {
-							$('.alert-dismissible').fadeOut('slow', function(){
-								$(this).remove();
-							});
-						}, 3000);
-					},
-					error: function (e) {
-						console.log(e)
-					}
-				});
-			});
-		});
-	} else {
-		$.ajax({
-			url: 'php/mail.php', // form action url
-			type: 'POST', // form submit method get/post
-			dataType: 'json', // request type html/json/xml
-			data: form.serialize(), // serialize form data
-			beforeSend: function () {
-				submit.attr("disabled", "disabled");
-				var loadingText = '<span role="status" aria-hidden="true" class="spinner-border spinner-border-sm align-self-center me-2"></span>Sending.....'; // change submit button text
-				if (submit.html() !== loadingText) {
-					submit.data('original-text', submit.html());
-					submit.html(loadingText);
-				}
-			},
-			success: function (data) {
-				submit.before(data.Message).fadeIn("slow"); // fade in response data 
-				submit.html(submit.data('original-text'));// reset submit button text
-				submit.removeAttr("disabled", "disabled");
-				if (data.response == 'success') {
-					form.trigger('reset'); // reset form
-				}
-				setTimeout(function () {
-					$('.alert-dismissible').fadeOut('slow', function(){
-						$(this).remove();
-					});
-				}, 3500);
-				if (typeof $('#recaptcha-v2').val() != "undefined") {
-					grecaptcha.reset(); // reset reCaptcha
-				}
-			},
-			error: function (e) {
-				console.log(e)
-			}
-		});
-	}
+	// if (typeof $('#google-recaptcha-v3').val() != "undefined") {
+	// 	grecaptcha.ready(function () {
+	// 		var site_key = $('#google-recaptcha-v3').attr('src').split("render=")[1];
+	// 		grecaptcha.execute(site_key, {action: 'contact'}).then(function (token) {
+	// 			var gdata = form.serialize() + '&g-recaptcha-response=' + token;
+	// 			$.ajax({
+	// 				url: 'php/mail.php',  // form action url
+	// 				type: 'POST', 		  // form submit method get/post
+	// 				dataType: 'json', 	  // request type html/json/xml
+	// 				data: gdata, 		  // serialize form data
+	// 				beforeSend: function () {
+	// 					submit.attr("disabled", "disabled");
+	// 					var loadingText = '<span role="status" aria-hidden="true" class="spinner-border spinner-border-sm align-self-center me-2"></span>Sending.....'; // change submit button text
+	// 					if (submit.html() !== loadingText) {
+	// 						submit.data('original-text', submit.html());
+	// 						submit.html(loadingText);
+	// 					}
+	// 				},
+	// 				success: function (data) {
+	// 					submit.before(data.Message).fadeIn("slow"); // fade in response data 
+	// 					submit.html(submit.data('original-text'));// reset submit button text
+	// 					submit.removeAttr("disabled", "disabled");
+	// 					if (data.response == 'success') {
+	// 						form.trigger('reset'); // reset form
+	// 					}
+	// 					setTimeout(function () {
+	// 						$('.alert-dismissible').fadeOut('slow', function(){
+	// 							$(this).remove();
+	// 						});
+	// 					}, 3000);
+	// 				},
+	// 				error: function (e) {
+	// 					console.log(e)
+	// 				}
+	// 			});
+	// 		});
+	// 	});
+	// } else {
+	// 	$.ajax({
+	// 		url: 'php/mail.php', // form action url
+	// 		type: 'POST', // form submit method get/post
+	// 		dataType: 'json', // request type html/json/xml
+	// 		data: form.serialize(), // serialize form data
+	// 		beforeSend: function () {
+	// 			submit.attr("disabled", "disabled");
+	// 			var loadingText = '<span role="status" aria-hidden="true" class="spinner-border spinner-border-sm align-self-center me-2"></span>Sending.....'; // change submit button text
+	// 			if (submit.html() !== loadingText) {
+	// 				submit.data('original-text', submit.html());
+	// 				submit.html(loadingText);
+	// 			}
+	// 		},
+	// 		success: function (data) {
+	// 			submit.before(data.Message).fadeIn("slow"); // fade in response data 
+	// 			submit.html(submit.data('original-text'));// reset submit button text
+	// 			submit.removeAttr("disabled", "disabled");
+	// 			if (data.response == 'success') {
+	// 				form.trigger('reset'); // reset form
+	// 			}
+	// 			setTimeout(function () {
+	// 				$('.alert-dismissible').fadeOut('slow', function(){
+	// 					$(this).remove();
+	// 				});
+	// 			}, 3500);
+	// 			if (typeof $('#recaptcha-v2').val() != "undefined") {
+	// 				grecaptcha.reset(); // reset reCaptcha
+	// 			}
+	// 		},
+	// 		error: function (e) {
+	// 			console.log(e)
+	// 		}
+	// 	});
+	// }
+	const name = $("#name").val();
+	const companyName = $("#companyName").val();
+	const description = $("#description").val();
+
+	console.log(name, companyName, description);
+
+	window.open("mailto:hi@outstride.com?subject=Founder%20Coaching:%20" + name + "%20|%20" + companyName + "&body=" + description)
+
 });
 
 })(jQuery)
